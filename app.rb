20107d7f11fa706c594de8c07db8a29e3e50c7a9
Grace Lu <155923103+ggracelu@users.noreply.github.com>
2025-04-22 04:21:17 +0000

    finished the rest

--- app.rb
+++ app.rb
@@ -18,6 +18,44 @@ get("/square/results") do
   erb(:square_results)
 end
 
-get("/squar_root/new") do
-  
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @num = params.fetch("number").to_f
+  @num_root = @num.to_f ** 0.5
+  erb(:root_results)
+end
+
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+  @minimum = params.fetch("min").to_f
+  @maximum = params.fetch("max").to_f
+  @result = rand(@minimum..@maximum)
+  erb(:random_results)
+end
+
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f
+  @r = (@apr/100)/12
+  @years = params.fetch("yrs").to_i
+  @months = @years * 12
+  @principal = params.fetch("princ").to_f
+
+  @numerator = @r * @principal
+  @denominator = 1 - (1 + @r)**(-@months)
+
+  @monthly_payment = @numerator/@denominator
+  erb(:payment_results)
 end
